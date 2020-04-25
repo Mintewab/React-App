3a44eada0519c67a3f773cc74996c2a57aa47d9a
Mintewab <demissiewminte123@gmail.com>
2020-04-24 22:46:41 -0700

    Updates

---
+++ precache-manifest.fcbe70daf2972b2699887e185e967ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debbb4df80e3f9d5054626d7ae051296",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "b14c8d3f9569bcfb0613",
+    "url": "/React-App/static/css/main.fb108a5e.chunk.css"
+  },
+  {
+    "revision": "f757cf264434f883c100",
+    "url": "/React-App/static/js/2.fcb612a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.fcb612a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14c8d3f9569bcfb0613",
+    "url": "/React-App/static/js/main.369c93a3.chunk.js"
+  },
+  {
+    "revision": "03b4549605d64572d1fb",
+    "url": "/React-App/static/js/runtime-main.85035b6a.js"
+  }
+]);
